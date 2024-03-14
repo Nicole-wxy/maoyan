124e8dd9564f4b25385d15212525dc9000333383
wuxiaoying <1121900174@qq.com>
2024-03-14 18:09:04 +0800

    dist update

--- vue.config.js
+++ vue.config.js
@@ -7,24 +7,24 @@ module.exports = defineConfig({
   pages : {
     // 配置不同的页面对应的入口 JS 文件
     index : {
-      entry : "src/views/IndexPage/main.js",
-      template : "public/index.html",
+      entry : "./src/views/IndexPage/main.js",
+      template : "./public/index.html",
     },
     video : {
-      entry : "src/views/VideoPage/main.js",
-      template : "public/video.html",
+      entry : "./src/views/VideoPage/main.js",
+      template : "./public/video.html",
     },
     shortvideo : {
-      entry : "src/views/ShortVideoPage/main.js",
-      template : "public/shortvideo.html",
+      entry : "./src/views/ShortVideoPage/main.js",
+      template : "./public/shortvideo.html",
     },
     performance : {
-      entry : "src/views/PerformancePage/main.js",
-      template : "public/performance.html",
+      entry : "./src/views/PerformancePage/main.js",
+      template : "./public/performance.html",
     },
     mine : {
       entry : "src/views/MinePage/main.js",
-      template : "public/mine.html",
+      template : "./public/mine.html",
     }
   }
 })
